663bb1f647f28ad99850c16ea2cd136f040406bb
Sam Watkins <sam@aiki.info>
2018-06-24 20:27:33 +1000

    .

--- kk-fix.sh
+++ kk-fix.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-< kk kut 2 | while read A; do T=$(hms- $A); echo $(hms $[$T-2]); done >kk2
+< kk kut 2 | while read A; do T=$(hms- $A); echo $(hms $[$T-4]); done >kk2
 < kk kutout 1 2 > kk3
 paste kk2 kk3 > kk4
